8eafb55acc021f607d37918c01c4b22c3036e920
estherdelpor <56587075+estherdelpor@users.noreply.github.com>
2019-10-21 19:38:27 +0100

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,8 @@
+name = ' '
+while name != 'marta':
+    print('please type your username with lowercase')
+    name = input( )
+
+print('how many guests are we having?')
+numofguests = int(input( ))
+print('Be sure to have enough room :)')
