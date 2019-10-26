a3b52df040e60a52c9927c3a85d2dc9749b81551
estherdelpor <estherdelpor@gmail.com>
2019-10-26 19:05:52 +0100

    change

---
+++ Week-3/python1.py
@@ -0,0 +1,8 @@
+name = ' '
+while name != 'marta':
+    print('please type your username with lowercase')
+    name = input( )
+
+print('how many guests are we having tomrrow?')
+numofguests = int(input( ))
+print('Be sure to have enough roo :)')
