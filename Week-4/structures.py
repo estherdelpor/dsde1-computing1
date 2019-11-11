2330b7155aabdd609f24a19e71263a3f7337419a
estherdelpor <estherdelpor@gmail.com>
2019-11-11 11:54:56 +0100

    half way done

--- Week-4/structures.py
+++ Week-4/structures.py
@@ -6,40 +6,59 @@ Simple functions performing operations on basic Python data structures.
 
 # Lists
 
-# write a function that returns a list containig the first and the last element
+# write a function that returns a list containing the first and the last element
 # of "the_list". 
 def first_and_last(the_list):
-    return []
+    list = [the_list[0] , the_list[-1]]
+    return [list]
 
 
 # write a function that returns part of "the_list" between indices given by the
 # second and third parameter, respectively. The returned part should be in
 # reverse order than in the original "the_list". 
-# If "end" is greater then "beginning" or any og the indices is out of the
+# If "end" is greater then "beginning" or any of the indices is out of the
 # list, raise a "ValueError" exception. 
 def part_reverse(the_list, beginning, end):
-    return # hint this is incomplete
+     if int(end) > int(beginning):
+        for x in the_list[beginning:end]:
+             return the_list.reverse() 
+    else:
+          raise ValueError
 
+    
+     # hint this is incomplete
 
 # write a function that at the "index" of "the_list" inserts three times the
 # same value. For example if the_list = [0,1,2,3,4] and index = 3 the function
 # will return [0,1,2,3,3,3,4]. 
 def repeat_at_index(the_list, index):
-    return
+     if index == the_list[x]:
+      the_list.insert(3, x)
+      return the_list
 
-
+def repeat_at_index(the_list, index):
+    the_list.insert(index, index)
+    the_list.insert(index, index)
+    the_list.insert(index, index)
+    return the_list
 # Strings
 
 # write a function that checks whether the word is a palindrome, i.e. it reads
 # the same forward and backwards
 def palindrome_word(word):
-    return
+    '''create the reverse of the word and compare'''
+    if word == word[::-1]:
+        return True
+    else: 
+        return False
 
 # write a function that checks whether the sentence is a palindrome, i.e. it
 # read the same forward and backward. Ignore all spaces and other characters
 # like fullstops, commas, etc. Also do not consider whether the letter is
 # capital or not. 
 def palindrome_sentence(sentence):
+    
+
     return
 
 # write a function that concatenates two sentences. First the function checks
@@ -58,14 +77,24 @@ def concatenate_sentences(sentenece1, sentence2):
 # write a function that checks whether there is a record with given key in the
 # dictionary. Return True or False.
 def index_exists(dictionary, key):
-    return
+    if key in dictionary:
+        return True
+
+    else:
+        return False
 
 # write a function which checks whether given value is stored in the
 # dictionary. Return True or False.
 def value_exists(dictionary, value):
-    return
+    for x in dictionary:
+        return True
+    
+    else:
+        return False
 
 # write a function that returns a new dictionary which contains all the values
 # from dictionary1 and dictionary2.
 def merge_dictionaries(dictionary1, dictionary2):
-    return
+   new_dict = { dictionary1, dictionary2}
+   
+   return new_dict
